4e160aa5f00401ac90863f3ffa6aff416012fcbf
caramelPopp <rampazibarbara1203@gmail.com>
2025-06-04 12:32:04 -0300

    iniciate routes structure

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class MainController extends Controller {
+    public static function home() {
+        return view('');
+    }
+
+    public static function courses() {
+        return view('');
+    }
+
+    public static function departments() {
+        return view('');
+    }
+
+    public static function opportunities() {
+        return view('');
+    }
+
+    public static function contact() {
+        return view('');
+    }
+}
